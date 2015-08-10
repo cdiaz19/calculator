bc73e9f86a28744dd7875bf556722dfda8ca47c6
cdiaz19 <cdiaz@pernix-solutions.com>
2015-08-09 18:24:11 -0600

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -2,22 +2,33 @@ require 'spec_helper'
 
 class Calculator
  def addition(v1,v2)
-  v1 + v2
+   add = v1 + v2
+   puts 'addition result is: #{add}'
  end
 
  def subtraction(v1,v2)
-   v1 - v2
+   if v1 > 0 && v2 > 0
+     sub = v1 - v2
+     puts 'subtraction result is: #{sub}'
+   else 
+     subsum = v1 + v2
+     puts 'subtraction result is: #{subsum}'
  end
 
  def multiplication(v1,v2)
-   v1 * v2
+   if v1 != 0 && v2 != 0
+     multi = v1 * v2
+     puts 'multiplication result is: #{multi}'
+   else
+     puts 'multiplication result for zero, is zero'
  end
 
  def division(v1,v2)
    if v2 != 0
-     v1 / v2
+     div = v1 / v2
+     
    else
-     puts'The division by zero, does not exist'
+     puts'the division by zero, does not exist'
    end
  end
 end
@@ -33,16 +44,16 @@ RSpec.describe Calculator do
 
    describe '#subtraction' do
     context 'when subtraction two number positve' do
-      it{ subject.subtraction(6,5)}
+      it{  subject.subtraction(6,5)}
     end
     context 'when subtraction two number negative' do
       it{ subject.addition(-5,-6) }
    end
   end
 
-  # describe '#multiplication()' do
-   #   expect( subject.multiplication(6,2) )
-  # end
+   describe '#multiplication()' do
+     expect( subject.multiplication(6,2) )
+   end
 
    describe '#division()' do
       context 'when division two number different zero' do
